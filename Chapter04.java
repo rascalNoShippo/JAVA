78aea63459519b12dfab5c65aa7c304524784011
rascalnoshippo <naofirefox@gmail.com>
2023-02-08 16:54:01 +0900

    JAVA

---
+++ Chapter04.java
@@ -0,0 +1,10 @@
+public class Chapter04 {
+	public static void main(String[] args) {
+		String name = "黒沢直";
+		int num = 7;
+		System.out.println(name);
+		name = "好きな言葉";
+		System.out.println(name);
+		System.out.println(num);
+	}
+}
